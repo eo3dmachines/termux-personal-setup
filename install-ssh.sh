cffd5b8c3b3ea0be2cbfd606f7440f96554ff5e0
eo3dmachines <ian.eric.ong@gmail.com>
2022-02-16 05:45:01 +0800

    Update and rename termux.sh to install-ssh.sh

--- install-ssh.sh
+++ install-ssh.sh
@@ -3,19 +3,27 @@
 
 #Install OpenSSH:
 echo Installing OpenSSH
+echo
 pkg install openssh
+echo
 
 echo Enter SSH password:
+echo
 passwd
+echo
 
 echo Checking username:
+echo
 whoami
+echo
 
 echo "Find the host by running this in Termux: (Look for something like inet addr:192.168.100.8)"
 ifconfig
+echo
 
 echo Starting SSH server:
 sshd
+echo
 
 echo To stop ssh, enter this command:
 echo pkill sshd
@@ -23,3 +31,5 @@ echo
 echo "On your computer, SSH into your machine on port 8022 (default port):"
 echo "ssh <username>@<host> -p 8022"
 echo "ex. ssh u0_a254@192.168.100.8 -p 8022"
+echo
+echo
